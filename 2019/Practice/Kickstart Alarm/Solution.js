3983ed18642e60afeff3889145659207037f05a9
gokartgo <gokart.007@hotmail.com>
2020-03-14 19:44:03 +0700

    pass example kickstart

--- 2019/Practice/Kickstart Alarm/Solution.js
+++ 2019/Practice/Kickstart Alarm/Solution.js
@@ -24,14 +24,10 @@ const answer = (N,K,x1,y1,C,D,E1,E2,F,question) => {
     let sum = 0
     for(let i=1;i<=K;i++) {
       for(let j=0;j<N;j++) {
-        let check = []
-        let a = 0
         for(let k=0; k<N-j; k++) {
           for(let l=k; l<=k+j; l++) {
             sum = ( sum + A[l] * Math.pow((l-k+1) ,i) ) % 1000000007
-            check[a++] = A[l]
           }
-          check[a++] = ' '
         }
       }
     }
@@ -44,15 +40,15 @@ rl.on('line', (line) => {
     expect = 'question';
   } else if (expect === 'question') {
     input_num = line.split(' ')
-    N[question] = input_num[0]
-    K[question] = input_num[1]
-    x1[question] = input_num[2]
-    y1[question] = input_num[3]
-    C[question] = input_num[4]
-    D[question] = input_num[5]
-    E1[question] = input_num[6]
-    E2[question] = input_num[7]
-    F[question] = input_num[8]
+    N[question] = parseInt(input_num[0])
+    K[question] = parseInt(input_num[1])
+    x1[question] = parseInt(input_num[2])
+    y1[question] = parseInt(input_num[3])
+    C[question] = parseInt(input_num[4])
+    D[question] = parseInt(input_num[5])
+    E1[question] = parseInt(input_num[6])
+    E2[question] = parseInt(input_num[7])
+    F[question] = parseInt(input_num[8])
     question++
     if(question === num_test_cases) {
         rl.close()
