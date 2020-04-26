f0f4b8f50d99230104f360e8d8099f91cd50f37f
gokartgo <gokart.007@hotmail.com>
2020-04-26 16:00:06 +0700

    add template of kick start

---
+++ template/1IQ1L_MID1L.js
@@ -0,0 +1,53 @@
+// 1 input question 1 line multiple input data 1 line
+// example input
+// 3
+// 3
+// 3 7 2
+// 4
+// 11 10 5 50
+// 1
+// 1
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+let expect = 'begin';
+let num_test_cases;
+let input = [];
+let num_data = [];
+let index_num_data = 0;
+let data = [];
+let index_data = 0;
+let question = 0;
+let check = 1
+
+const answer = (n) => {
+    
+    console.log(`Case #${n}: `)
+}
+
+rl.on('line', (line) => {
+  if (expect === 'begin') {
+    num_test_cases = parseInt(line);
+    expect = 'question';
+  } else if (expect === 'question') {
+    if(check % 2 === 1) {
+        input = line
+        num_data[index_num_data++] = parseInt(input)
+        check = 2
+    } else {
+        data[index_data++] = line.split(' ')
+        check = 1
+    }
+    question++
+    if(question === num_test_cases*2) {
+        rl.close()
+    }
+  }
+}).on('close',() => {
+    for(let i=0;i<num_test_cases;i++) {
+        answer(i+1)
+    }
+    process.exit(0);
+});
