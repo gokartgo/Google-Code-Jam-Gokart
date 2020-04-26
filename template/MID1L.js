6499b18554b24de3425adbd5f431534d99c17432
gokartgo <gokart.007@hotmail.com>
2020-04-26 15:58:31 +0700

    add template of kick start

---
+++ template/MID1L.js
@@ -0,0 +1,38 @@
+// many input data 1 line
+// 4
+// SSSEEE
+// N
+// N3(S)N2(E)N
+// 2(3(NW)2(W2(EE)W))
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+let expect = 'begin';
+let num_test_cases;
+let data = [];
+let index_data = 0;
+let question = 0;
+
+const answer = (n) => {
+  console.log(`Case #${n}: `)
+}
+
+rl.on('line', (line) => {
+  if (expect === 'begin') {
+    num_test_cases = parseInt(line);
+    expect = 'question';
+  } else if (expect === 'question') {
+    data[index_data++] = line.split('')
+    question++
+    if(question === num_test_cases) {
+        rl.close()
+    }
+  }
+}).on('close',() => {
+    for(let i=0;i<num_test_cases;i++) {
+        answer(move[i],0,1,i+1)
+    }
+    process.exit(0);
+});
