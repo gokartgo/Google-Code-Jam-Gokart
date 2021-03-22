e83569cbf80ce71b84922a99743a7bf0e9097a1d
gokartgo <gokart.007@hotmail.com>
2021-03-22 09:36:51 +0700

    finish k-goodness kick start A

---
+++ 2021/Kick Start/A/k-goodness_string.js
@@ -0,0 +1,53 @@
+const { match } = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+let expect = 'begin';
+let num_test_cases;
+let input = [];
+let num_data = [];
+let index_num_data = 0;
+let find = [];
+let index_find = 0;
+let data = [];
+let index_data = 0;
+let question = 0;
+let check = 1
+
+const answer = (n, num_data, find, data) => {
+    let good_ness = 0
+    for(let i=0;i < Math.floor(num_data / 2); i++) {
+      if(data[i] !== data[num_data - 1 - i]) {
+        good_ness++
+      }
+    }
+    console.log(`Case #${n}:`,Math.abs(find - good_ness))
+}
+
+rl.on('line', (line) => {
+  if (expect === 'begin') {
+    num_test_cases = parseInt(line);
+    expect = 'question';
+  } else if (expect === 'question') {
+    if(check % 2 === 1) {
+        input = line.split(' ')
+        num_data[index_num_data++] = parseInt(input[0])
+        find[index_find++] = parseInt(input[1])
+        check = 2
+    } else {
+        data[index_data++] = line.split(' ')
+        check = 1
+    }
+    question++
+    if(question === num_test_cases*2) {
+        rl.close()
+    }
+  }
+}).on('close',() => {
+    for(let i=0;i<num_test_cases;i++) {
+        answer(i+1,num_data[i], find[i], data[i][0])
+    }
+    process.exit(0);
+});
